7d051ea7b165e5322bb10a3dba0f95635b10ccdc
zyerin <zyerin5974@gmail.com>
2025-06-17 22:13:51 +0900

    feat: 관리자 페이지에 모델 추가 등록
    - admin 페이지에 Manager, Engineer, SupportRequest, DeviceStatus 모델 추가

--- bems/admin.py
+++ bems/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Phonebook
+from .models import Manager, Engineer, SupportRequest, DeviceStatus
 
 # Register your models here.
 
-admin.site.register(Phonebook)
+admin.site.register(Manager)
+admin.site.register(Engineer)
+admin.site.register(SupportRequest)
+admin.site.register(DeviceStatus)
