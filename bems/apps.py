06ffe125b37285bd6cc5805c94d6583aa03c9609
zyerin <zyerin5974@gmail.com>
2025-06-17 22:23:25 +0900

    feat: 앱 시작 시 TCP 서버 자동 실행 기능 추가
    - Django 서버 실행과 동시에 start_tcp_server() 호출하여 TCP 서버 백그라운드에서 자동 실행되도록 설정

--- bems/apps.py
+++ bems/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BemsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "bems"
+
+    def ready(self):
+        from .views.tcp_control import start_tcp_server
+        start_tcp_server()  # tcp 서버 시작! (서버가 실행되면서 자동으로 TCP 서버도 백그라운드로 실행)
