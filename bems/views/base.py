9d976b440ba0a3e516b78b36d75efae45a091560
zyerin <zyerin5974@gmail.com>
2025-06-17 22:16:17 +0900

    feat: 전화번호부 뷰에 Engineer 데이터 추가 및 컨텍스트 확장
    - 템플릿에서 관리자(Manager)뿐 아니라 엔지니어(Engineer) 정보도 함께 사용할 수 있도록 context를 확장

--- bems/views/base.py
+++ bems/views/base.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from ..models import Manager
+from ..models import Manager, Engineer
 
 # 기본 메인(테스트) 페이지
 def main_view(request):
@@ -8,12 +8,15 @@ def main_view(request):
 
 # 전화번호부를 화면에 출력하는 뷰
 def phonebook_view(request):
-    # 모든 Manager 항목을 가져옴
-    phonebook_entries = Manager.objects.all()
+    # 모든 Manager, Engineer 객체(항목) 가져옴
+    managers = Manager.objects.all()
+    engineers = Engineer.objects.all()
 
     # 템플릿에 전달할 데이터 context 구성
     context = {
-        'phonebook_entries': phonebook_entries,
+        'managers': managers,
+        'engineers': engineers
+
     }
 
     return render(request, 'bems/phonebook.html', context) # 템플릿 렌더링
