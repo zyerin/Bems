9e2b6215411593f199bddc3fe2f4fbd5696ed327
zyerin <zyerin5974@gmail.com>
2025-06-17 22:18:55 +0900

    feat: views 폴더 내 뷰 모듈을 통합 관리하기 위한 __init__.py 추가

---
+++ bems/views/__init__.py
@@ -0,0 +1,6 @@
+from .base import main_view, phonebook_view
+from .sms import send_sms_view
+from .engineer import engineer_view, engineer_login, engineer_approved_view, engineer_request_approval
+from .manager import manager_view, manager_response, manager_login
+from .tcp_control import control, control_device
+from .logs import view_logs
